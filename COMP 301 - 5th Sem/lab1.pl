df4219ba434b1391bf3325b81bec487920f38a74
Sambeg Shrestha <sthasam2@gmail.com>
2021-06-09 09:30:03 +0545

    COMP 301 (5th Sem) Addition

---
+++ COMP 301 - 5th Sem/lab1.pl
@@ -0,0 +1,23 @@
+/*
+FACTS
+*/
+
+% Relationships
+parent(pam, bob).
+parent(tom, bob).
+parent(tom, liz).
+parent(bob, ann).
+parent(bob, pat).
+parent(pat, jim).
+
+
+%  Gender
+female(pam).
+male(tom).
+male(bob).
+female(pat).
+male(jim).
+female(liz).
+female(ann).
+
+
